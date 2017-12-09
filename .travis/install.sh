3f6b7478f85bb419d7107f1a84422377e33ca86a
kunicmarko20 <kunicmarko20@gmail.com>
2017-12-09 23:06:11 +0100

    allow Symfony4

--- .travis/install.sh
+++ .travis/install.sh
@@ -5,9 +5,6 @@ mkdir --parents "${HOME}/bin"
 
 composer global require "squizlabs/php_codesniffer"
 
-wget "https://phar.phpunit.de/phpunit-5.7.phar" --output-document="${HOME}/bin/phpunit"
-chmod u+x "${HOME}/bin/phpunit"
-
 # Coveralls client install
 wget https://github.com/satooshi/php-coveralls/releases/download/v1.0.1/coveralls.phar --output-document="${HOME}/bin/coveralls"
 chmod u+x "${HOME}/bin/coveralls"
